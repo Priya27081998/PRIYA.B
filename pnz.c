252a6f3e416060e21cabeffcf9ef09f99eb9f9bb
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-01-18 15:50:44 +0530

    pnz.c

---
+++ pnz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+ int n;
+ printf("enter a number:");
+ if(n>0)
+ {
+ printf("it is positive");
+ }
+ else(n<0)
+ {
+  printf("it is negative");
+  }
+  else if(n=0)
+  {
+   printf("it is zero");
+   }
+   return 0;
+   }
