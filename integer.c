438872c8351eb676a10bf0c7fa2c90ee18872815
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-01-19 16:50:42 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+ int lim,a[50],i,n,sum=0;
+ printf("enter the number");
+ scanf("%d",&lim);
+ for(i=0;i<lim;i++)
+ {
+  scanf("%d",&a[i]);
+  }
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+  sum=sum+i;
+  }
+  print("sum is %d");
+  }
