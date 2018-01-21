fe02ffde06f899cebd93500f6914d67dbe7f3098
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-01-21 05:00:11 +0000

    Create oddint.c

---
+++ oddint.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ int num,m=20,n=40;
+ clrscr();
+ printf("print odd number in a given range m to n:/n");
+ for(num=m;num<=n;num++)
+ {
+  if(num%2==1)
+  {
+   printf("%d",num);
+   }
+   getch();
+   }
+   }
