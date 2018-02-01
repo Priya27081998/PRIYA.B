c9dbfbd81d20733ad15448d4ef4a4ff21ce4f70e
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-02-01 10:25:19 +0000

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+ char n;
+ printf("enter the string:");
+ scanf("%c",&n);
+ if((65<=n>=90)||(97<=n>=122))
+ {
+  printf("no");
+  }
+  else
+  {
+   printf("yes");
+   }
+   return 0;
+   }
