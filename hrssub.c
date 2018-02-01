d86aed4b88a570ad7e8cecb937d2b9ed63b6ea5b
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-02-01 11:13:57 +0000

    Create hrssub.c

---
+++ hrssub.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int hur,min,t1,t2,t3;
+ printf("enter the time 1:");
+ scanf("%d",&t1);
+ printf("enter the time 2:");
+ scanf("%d \n",&t2);
+ printf("remain time:");
+ t3=t1-t2;
+ printf("%d \n",t3);
+ }
+ 
+ 
+ 
+ 
+ 
+ 
