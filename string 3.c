c59f8d36e384ec9f6a4a9be54b685ee5015596c6
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-02-08 09:02:03 +0000

    Create string 3.c

---
+++ string 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int n,count=0,i;
+ printf("enter the number of times:");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+  count++;
+  printf("\n laptop);
+  }
+  return 0;
+  }
+ 
+ 
