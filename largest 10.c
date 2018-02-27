c0b44149f494a56c299abad71a625ec8d9594bc9
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-02-27 16:51:29 +0530

    Create largest 10.c

---
+++ largest 10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,max,data[10];
+for(i=0;i<10;i++)
+scanf("%d",data[i]);
+max=data[0];
+for(i=1;i<10;i++)
+if(dta[i]>max)max=data[i];
+printf("%d",max);
+return0;
+}
