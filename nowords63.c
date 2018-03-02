481cbc9c7841dfda2ef123ab1d3107371a74999e
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-03-02 16:15:06 +0530

    Create nowords63.c

---
+++ nowords63.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+ int i,count=1;
+ char s[30];
+ printf("enter the string:");
+ scanf("%[^\n]s",&s);
+  if(s[i]=' ')
+  {
+   count=count+1;
+   }
+   printf("no of words:%d",count);
+   }
