977c98ed10901e7116d952d07da313cedebb615e
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-03-02 16:22:45 +0530

    Create sum64.c

---
+++ sum64.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int n,m;
+ printf("enter the num:");
+ scanf("%d %d",&n,&m);
+ printf("sum:");
+ sum=m+n;
+ printf("%d",sum);
+ if(sum/2==0)
+ {
+  printf("even");
+  }
+  else
+  {
+   printf("odd");
+   }
+   }
