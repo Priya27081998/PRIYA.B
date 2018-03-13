8be1bf90430b00b41aa26fd2d8eedca5e1e20bb8
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-03-13 16:59:44 +0530

    Create perfectsq.c

---
+++ perfectsq.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter two num:");
+scanf("%d %d",&a,&b);
+printf("product is:%d",a*b);
+if(a/b=1)
+{
+ printf("perfect square");
+ }
+ else
+ {
+ printf("not perfect square");
+ }
+ return 0;
+ }
