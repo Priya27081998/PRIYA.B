4b64290b1c6201a2b62ba077e44d53bce53f6243
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-03-13 15:49:46 +0530

    Create LR73.c

---
+++ LR73.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int n,l,m;
+ printf("enter the num:");
+ scanf("%d",&n);
+ printf("between the range of:");
+ scanf("%d",&l,&m);
+ if(n>l&&n<m)
+ {
+  printf("yes");
+  }
+  else
+  {
+   printf("no");
+   }
+   return 0;
+   }
