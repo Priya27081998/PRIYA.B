a91e6d7972d964ad10c9054dfdeaa4590be518f8
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-03-20 16:10:57 +0530

    Create divmod83.c

---
+++ divmod83.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int n1,n2,div,mod;
+ printf("enter two num:");
+ scanf("%d %d",&n1,&n2);
+ div=n1/n2;
+ printf("\n %d",div);
+ mod=n1%n2;
+ printf("\n %d",mod);
+ return 0;
+ }
