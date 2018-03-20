73c0714ff46f2c5d036c32af8dd9a8d4a1e62a9b
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2018-03-20 15:53:27 +0530

    Create area82.c

---
+++ area82.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ float n1,n2,area;
+ printf("enter the length:");
+ scanf("%f",&n1);
+ printf("enter the breadth:");
+ scanf("%f",&n2);
+ area=n1*n2;
+ printf("%f",area);
+ }
